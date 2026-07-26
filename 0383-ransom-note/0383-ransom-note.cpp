8f6489bd554d456ff21812c0493b0076c37b9569
Kamranahmadrao1014 <kamranahmadrao1014@gmail.com>
2026-07-26 13:29:12 +0500

    Time: 3 ms (52.58%), Space: 12.1 MB (11.95%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>magazine_map;
+        for(int i=0;i<magazine.length();i++){
+            magazine_map[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            auto it=magazine_map.find(ransomNote[i]);
+            if(magazine_map[ransomNote[i]] == 0){
+                return false;
+            }else{
+                magazine_map[ransomNote[i]]--;
+            }
+        }
+        return true;
+    }
+};
